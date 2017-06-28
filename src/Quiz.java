c9f7d97b1948d45024d4afb10879702a5b36fe77
Ana Gurgenidze <ana.gurgenidze11@gmail.com>
2017-06-28 19:59:35 +0400

    update

--- src/Quiz.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by anophoo on 6/28/17.
- */
-public class Quiz {
-}
--- src/temp.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kdufla on 6/7/17.
- */
-public class temp {
-    public static void main(String[] args) {
-        System.out.println("hhh");
-    }
-}
