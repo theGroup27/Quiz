448517b16d604c2c4104e8111a34e96b2a559d6f
Ana Gurgenidze <ana.gurgenidze11@gmail.com>
2017-07-08 20:59:51 +0400

    Created QuestionServlet

---
+++ src/question/QuestionServlet.java
@@ -0,0 +1,19 @@
+package question;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * Created by anophoo on 7/8/17.
+ */
+@WebServlet("/QuestionServlet")
+public class QuestionServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+}
