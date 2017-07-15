9cfc8eceff9b23929883b1d9f7bfaa933987b2e4
Jiio Marcy <gdane14@freeuni.edu.ge>
2017-07-16 02:58:56 +0400

    ignore

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -15,26 +15,6 @@ import java.util.List;
  */
 public class DBConnection {
 
-
-    //returns list of all users from database
-//    private List<User> getAllUsers(){
-//        List<User> res = new ArrayList<User>();
-//
-//        connection conn = new connection();
-//
-//        conn.call();
-//        conn.sendStatement("Select * from users");
-//
-//        while(conn.isNext())
-//            res.add(new User(Integer.parseInt(conn.getString(1)), conn.getString(2), conn.getString(3)));
-//
-//        return res;
-//    }
-
-    //    /*
-//    inner class to connect to mysql, send statements and to keep track of result set
-//     */
-//    class connection {
     private String server = MyDBInfo.MYSQL_DATABASE_SERVER;
     private String username = MyDBInfo.MYSQL_USERNAME;
     private String password = MyDBInfo.MYSQL_PASSWORD;
@@ -77,58 +57,3 @@ public class DBConnection {
 
 }
 
-
-//        public void call() {
-//            try {
-//                Class.forName("com.mysql.jdbc.Driver");
-//                Connection con = DriverManager.getConnection("jdbc:mysql://" + server, username, password);
-//
-//                Statement stmt = (Statement) con.createStatement();
-//                stmt.executeQuery("USE " + database);
-//            } catch (SQLException | ClassNotFoundException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        public void sendStatement(String statementString) {
-//            try {
-//                statement = (Statement) con.createStatement();
-//                rs = statement.executeQuery(statementString);
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//
-//        public boolean isNext() {
-//            boolean isThereNext = false;
-//            try {
-//                isThereNext = rs.next();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//
-//            return isThereNext;
-//        }
-//
-//        public String getString(int index) {
-//            String res = null;
-//
-//            try {
-//                res = rs.getString(index);
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//
-//            return res;
-//        }
-//
-//        public void close() {
-//            try {
-//                con.close();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-////    }
-//}
